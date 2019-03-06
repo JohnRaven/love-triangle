1d4d80d8032ad44c493125b65fb06f5d345a5c50
JohnRaven <John-B-RAven@yandex.ru>
2019-03-06 23:53:45 +0300

    feat: added function which find love-triangle

--- src/index.js
+++ src/index.js
@@ -4,4 +4,15 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let x, y, z, count = 0;
+
+  for (let i = 0; i < preferences.length; i++) {
+    x = preferences[i];
+    y = preferences[x - 1];
+    z = preferences[y - 1];
+    if (x != y && y != z && z == (i+1)) {
+      count++;
+    }
+  }
+  return count/3;
 };
